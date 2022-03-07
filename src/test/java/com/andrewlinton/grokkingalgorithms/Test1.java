d2ee7369b93d4d6901c854d700385f657472bd9b
ajlinton1 <ajlinton@hotmail.com>
2022-03-07 07:37:03 -0800

    Initial

---
+++ src/test/java/com/andrewlinton/grokkingalgorithms/Test1.java
@@ -0,0 +1,11 @@
+package com.andrewlinton.grokkingalgorithms;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+    @Test
+    public void test1() {
+        System.out.println("test1");
+    }
+}
